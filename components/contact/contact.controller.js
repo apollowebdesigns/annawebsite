e8d0f675f96aae071bc5f74f2efabb18a8d97745
Andy Evans <andrewevans0@outlook.com>
2017-11-21 21:23:57 +0000

    got http for lambda into the project

--- components/contact/contact.controller.js
+++ components/contact/contact.controller.js
@@ -3,18 +3,32 @@
         .module('app')
         .controller('ContactController', ContactController);
 
-    ContactController.$inject = ['$scope'];
+    ContactController.$inject = ['$scope', '$http'];
 
-    function ContactController($scope) {
+    function ContactController($scope, $http) {
         var vm = this;
 
         // function to submit the form after all validation has occurred
         vm.submitForm = function(isValid) {
 
             // check to make sure the form is completely valid
-            if (isValid) {
-                alert('our form is amazing' + $scope.user + ' is this!!!!');
-            }
+            console.log('user form sent successfully');
+            console.log($scope.user);
+
+            var url = 'https://uyolovpg91.execute-api.us-west-2.amazonaws.com/prod/contactHandler';
+
+            $http({
+                method: 'GET',
+                url: url,
+                headers:
+            }).then(function successCallback(response) {
+                // this callback will be called asynchronously
+                // when the response is available
+                console.log(response);
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
 
         };
     }
